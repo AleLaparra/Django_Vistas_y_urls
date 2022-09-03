838c6849f2eaa9829a3a71c3a41c4f942abd9c1d
AleLaparra <jessilaparra16@hotmail.com>
2022-09-03 01:07:00 -0600

    Creacion de vistas

--- Apps/home/migrations/urls.py
+++ Apps/home/migrations/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from tarea1.views import home
-
+from django.urls import path, include 
+from Apps.home import views
+from .views import HomeView
 urlpatterns = [
 
-
+path('',HomeView.as_view(), name='homeapp')
 
 
 ]
